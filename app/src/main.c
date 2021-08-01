b710d96cfac445d10e9080ae4f4c1a9e13cce128
hatheviet7121996@gmail.com <hatheviet7121996@gmail.com>
2021-08-01 13:21:58 +0700

    Clean up

--- app/src/main.c
+++ app/src/main.c
@@ -564,7 +564,7 @@ static void MX_USART3_UART_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN USART3_Init 2 */
-	huart2.Instance->BRR = (0x2d9);
+	huart3.Instance->BRR = (0x2d9);
   /* USER CODE END USART3_Init 2 */
 
 }
